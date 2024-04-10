6872bc3b25986b3bf2ff0e5d96ff15272501ab81
Weijia <xiao.wei@northeastern.edu>
2024-04-10 18:41:53 -0400

    add click SFX/improve on UI

---
+++ Assets/script/ButtonClick.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+
+    public AudioClip buttonSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClickSound()
+    {
+        // Play the click sound effect
+        print("play click sfx");
+        AudioSource.PlayClipAtPoint(buttonSFX, Camera.main.transform.position, 1);
+    }
+}
--- Assets/script/UserSettings.cs
+++ Assets/script/UserSettings.cs
@@ -6,11 +6,24 @@ using UnityEngine.UI;
 public class UserSettings : MonoBehaviour
 {
     public static float mouseSensitivity;
+    public static float soundVolume;
 
     public Slider mouseSensitivitySlider;
+    public Slider soundVolumeSlider;
+
+    private void Start()
+    {
+        UpdateMouseSensitivity();
+        UpdateSoundVolume();
+    }
 
     public void UpdateMouseSensitivity()
     {
         mouseSensitivity = mouseSensitivitySlider.value;
+    }
+
+    public void UpdateSoundVolume()
+    {
+        soundVolume = soundVolumeSlider.value;
     }
 }
