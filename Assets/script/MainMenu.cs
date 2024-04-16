baca1c214f4cb418cc7d1641de8d269c3cf0d203
me1111o <yang.ruix@northeastern.edu>
2024-04-16 07:05:22 -0400

    delete Debug.log

--- Assets/script/MainMenu.cs
+++ Assets/script/MainMenu.cs
@@ -9,19 +9,14 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        // 在控制台中输出调试信息
-        Debug.Log("StartGame called.");
-
         if (PlayerPrefs.HasKey("LevelNumber"))
         {
             int levelToLoad = PlayerPrefs.GetInt("LevelNumber");
-            // 输出将要加载的关卡索引
-            Debug.Log("Loading saved level: " + levelToLoad);
+            
             SceneManager.LoadScene(levelToLoad);
         }
         else
         {
-            Debug.Log("No saved level, loading next level.");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
